b6916d7e94140d8e999db6fffd2e8f37f31900c9
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2022-05-27 21:22:18 +0800

    Update 3.0.py

--- 3.0.py
+++ 3.0.py
@@ -2,25 +2,26 @@
 import cv2,threading,tkinter,time,csv
 from pyzbar.pyzbar import decode
 
-testtestdict = {}
-stopcontrol = 'continue'
-
-def decode(waittime=0):
+# indentify code and return content of it
+def identifycode(waittime=0):
     capture = cv2.VideoCapture(0)
-    
     while True:
         ret,frame = capture.read()
-
         if ret == True:
             barcode = decode(frame, symbols=None)
-
             for content in barcode:
                 codedata = content.data.decode('utf-8')
                 codetype = content.type
-                printout = '{} ({})'.format(codedata, codetype)
+                print('success type {} data {}'.format(codetype, codedata))
                 return(codedata)
-                break
-
         else:
-            print('read capture fail')
-        time.sleep_ms(waittime)
+            print('fail identifycode')
+            break
+        cv2.imshow('Capture',frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        time.sleep(waittime/1000)
+
+
+ksksks = identifycode(0)
+print(ksksks)
