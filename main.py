cf0c32184650bdf8113db90e8ec09eca8a3a699d
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2022-06-08 11:20:43 +0800

    Update

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 # needs opencv_python, pyzbar
+from ast import While
+from cgi import test
 import cv2,threading,tkinter,time,csv
 from pyzbar.pyzbar import decode
 
+# take one picture
+def Capture():
+    capture = cv2.VideoCapture(0)
+    ret, frame = capture.read()
+    if ret == True:
+        return(frame)
+    else:
+        #loglog
+        print()
+
 # indentify code and return content of it
-def identifycode(waittime=0):
+def DeQrCode():
     capture = cv2.VideoCapture(0)
     while True:
-        ret,frame = capture.read()
+        ret, frame = capture.read()
         if ret == True:
-            barcode = decode(frame, symbols=None)
-            for content in barcode:
-                codedata = content.data.decode('utf-8')
-                codetype = content.type
-                print('success type {} data {}'.format(codetype, codedata))
-                return(codedata)
-        else:
-            print('fail identifycode')
-            break
-        cv2.imshow('Capture',frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
-        time.sleep(waittime/1000)
+            rawCodes = decode(frame, symbols=None)
+            for codeContent in rawCodes:
+                codeData = codeContent.data.decode('utf-8')
+                codeType = codeContent.type
+                print('success type {} data {}'.format(codeType, codeData))
+                return(codeData)
+    #need improve
+'''
+    cv2.imshow('Capture',frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+'''
 
-
-ksksks = identifycode(0)
-print(ksksks)
+test = Capture()
+print(test)
+test2 = DeQrCode()
+print(test2)
