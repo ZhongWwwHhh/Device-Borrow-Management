31cc1ca2d5dac2321f29a0df96840292cce129e0
ZhongWwwHhh <79801664+ZhongWwwHhh@users.noreply.github.com>
2022-06-14 15:13:19 +0800

    Update

--- DeviceManagementApp.py
+++ DeviceManagementApp.py
@@ -9,16 +9,7 @@ from kivy.properties import ObjectProperty, StringProperty
 
 
 class MainMenu(FloatLayout):
-    '''Create a controller that receives a custom widget from the kv lang file.
-
-    Add an action to be called from the kv lang file.
-    '''
-    label_wid = ObjectProperty()
-    info = StringProperty()
-
-    def do_action(self):
-        self.label_wid.text = 'My label after button press'
-        self.info = 'New info text'
+    pass
 
 
 class DeviceManagementApp(App):
